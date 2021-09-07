ca5d9344abc0072b838e61edc706980d43070830
Denis Butsky <nthbutsky@gmail.com>
2021-09-07 18:46:56 +0300

    fix responsiveness

--- js/cahee-nav-menu-var-2-16.js
+++ js/cahee-nav-menu-var-2-16.js
@@ -1,9 +1,25 @@
+// MENU
+
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
-
-hamburger.addEventListener("click", showMobileMenu);
 
 function showMobileMenu() {
   hamburger.classList.toggle("header__nav-menu-btn_active");
   navMenu.classList.toggle("header__nav-list_active");
 }
+
+hamburger.addEventListener("click", showMobileMenu);
+
+// SCROLL ICON
+
+const scrollIcon = document.querySelector("#scroll-icon");
+
+function hideShowScrollIcon() {
+  if (window.scrollY > 0) {
+    scrollIcon.style.display = "none";
+  } else {
+    scrollIcon.style.display = "block";
+  }
+}
+
+window.addEventListener("scroll", hideShowScrollIcon);
