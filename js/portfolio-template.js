4f3db563e1ba63d3ab58c92b13716ae9622b4402
Denis Butsky <nthbutsky@gmail.com>
2021-09-08 15:32:02 +0300

    redo with scss, fix responsiveness

--- js/portfolio-template.js
+++ js/portfolio-template.js
@@ -1,4 +1,4 @@
-/* COOKIES MODAL */
+// COOKIES MODAL
 
 const cookiesModal = document.querySelector("#cookies-modal");
 const cookiesBtn = document.querySelector("#cookies-accept-btn");
@@ -12,19 +12,16 @@ document.addEventListener("DOMContentLoaded", () =>
 
 cookiesBtn.addEventListener("click", () =>
   setTimeout(function () {
-    cookiesModal.style.top = "-50px";
+    cookiesModal.style.top = "-100px";
   }, 500)
 );
 
-/* NAV MENU */
+// NAV MENU
 
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 const navMenuBg = document.querySelector("#nav-menu-bg");
 const closeBtn = document.querySelector("#menu-close-btn");
-
-hamburger.addEventListener("click", showHideMenu);
-closeBtn.addEventListener("click", showHideMenu);
 
 function showHideMenu() {
   hamburger.classList.toggle("header-section__nav-menu-btn-bar_hide");
@@ -32,7 +29,10 @@ function showHideMenu() {
   navMenuBg.classList.toggle("header-section__menu-transparent-bg_active");
 }
 
-/* HIRE MODAL */
+hamburger.addEventListener("click", showHideMenu);
+closeBtn.addEventListener("click", showHideMenu);
+
+// HIRE MODAL
 
 const hireModal = document.querySelector("#hire-modal");
 const hireModalBg = document.querySelector("#hire-modal-bg");
@@ -40,12 +40,26 @@ const hireModalOpenBtn = document.querySelector("#header-hire-btn");
 const hireModalCloseBtn = document.querySelector("#hire-modal-close-btn");
 const hireModalOpenBtnFooter = document.querySelector("#footer-hire-btn");
 
-hireModalOpenBtn.addEventListener("click", showHideHireModal);
-hireModalOpenBtnFooter.addEventListener("click", showHideHireModal);
-hireModalCloseBtn.addEventListener("click", showHideHireModal);
-
 function showHideHireModal() {
   hamburger.classList.toggle("header-section__nav-menu-btn-bar_hide");
   hireModal.classList.toggle("hire-modal-section__form_active");
   hireModalBg.classList.toggle("hire-modal-section__transparent-bg_active");
 }
+
+hireModalOpenBtn.addEventListener("click", showHideHireModal);
+hireModalOpenBtnFooter.addEventListener("click", showHideHireModal);
+hireModalCloseBtn.addEventListener("click", showHideHireModal);
+
+// SCROLL ICON
+
+const scrollIcon = document.querySelector("#scroll-icon");
+
+function hideShowScrollIcon() {
+  if (window.scrollY > 0) {
+    scrollIcon.style.display = "none";
+  } else {
+    scrollIcon.style.display = "block";
+  }
+}
+
+window.addEventListener("scroll", hideShowScrollIcon);
